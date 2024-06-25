162e6614590738b34109216370c97816e7ae8e72
Aiko781 <ptitaiko.20031@gmail.com>
2024-06-25 19:35:58 +0200

    Remove comments

--- src/Repository/CryptocurrenciesRepository.php
+++ src/Repository/CryptocurrenciesRepository.php
@@ -26,29 +26,4 @@ class CryptocurrenciesRepository extends ServiceEntityRepository
         $crypto = $this->find($id);
         return $crypto ? $crypto->getCryptoName() : null;
     }
-
-    //    /**
-    //     * @return Cryptocurrencies[] Returns an array of Cryptocurrencies objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Cryptocurrencies
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
